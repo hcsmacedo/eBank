8509fe1d34ade11e6cd5b3bae49229a6eef0827b
Heitor <heitorcsmacedo@gmail.com>
2021-10-31 09:08:07 -0300

    Business DataAnnotations;

--- src/eBank.Business.DTO/DTO/BankDTO.cs
+++ src/eBank.Business.DTO/DTO/BankDTO.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace eBank.Business.DTO.DTO
 {
     public class BankDTO
     {
+        [Required(ErrorMessage = "O campo 'Nome' é obrigatório.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "O campo 'Código do Banco' é obrigatório.")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "O código do banco deve ser formado por 3 dígitos.")]
         public string BankCode { get; set; }
+        [Required(ErrorMessage = "O campo 'Ativo' é obrigatório.")]
         public bool Active { get; set; }
     }
 }
